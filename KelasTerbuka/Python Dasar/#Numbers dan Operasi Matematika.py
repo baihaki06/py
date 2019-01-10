d5c0ee9fab85a2b8e82392a553450ba9a99296c1
baihaki06 <baihaki0607@gmail.com>
2019-01-10 11:58:31 +0700

    number and math

---
+++ KelasTerbuka/Python Dasar/#Numbers dan Operasi Matematika.py
@@ -0,0 +1,17 @@
+a = 3
+b = 8
+c = 4.0
+
+#penjumlahan
+print('penjumlahan a + b = ', a + b)
+
+#pengurangan
+print('pengurangan b - a = ', b - a )
+
+#perkalian
+print('perkalian a * b = ', a * b)
+
+#pembagian
+print('pembagian b / a = ', b / a)
+
+
