3d79c5893bd8973813dcbefca28c6ef7a138b2b5
baihaki06 <baihaki0607@gmail.com>
2019-01-10 16:51:23 +0700

    add string

---
+++ KelasTerbuka/Python Dasar/string.py
@@ -0,0 +1,50 @@
+text1 = 'jalan-jalan di hari minggu'
+
+text2 = 'jalan-jalan di  hari jum\'at'
+
+text3 = "jalan-jalan di hari jum'at"
+
+text4 = 'dia berkata "kemaren kemana bro?"'
+
+text5 = "saya menjawab \"jalan-jalan bro\""
+
+text6 = 'dia : "kemaren kemana bro?" \nSaya: "jalan-jalan bro"'
+
+print(text1)
+print(text2)
+print(text3)
+print(text4)
+print(text5)
+print(text6)
+
+
+text7 = ''' 
+dia : belajar apa coy?
+gue : lagi ngulik coding bro
+dia: ngoding mulu coy, gak capek apa?
+gue : cari nafkah bro
+''' 
+
+print(text7)
+
+text8 = """
+ini adalah cerita jaman dulu ketika 
+thunami aceh
+akibatnya   ratusan ribu nyawa melayang
+dan ada hikmah yang baik juga bagi aceh
+terjadinya perdamaian antara RI dan Gam
+"""
+
+print(text8)
+
+
+text9 = r'C\nyoto'
+
+print(text9)
+
+
+print(10*'wk')
+
+print('kue' ' pukis')
+
+print(text1 + text2)
