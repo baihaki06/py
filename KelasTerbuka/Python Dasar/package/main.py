a756ee965164974ee8c0418c8a8ddabe2b7d7f66
baihaki06 <baihaki0607@gmail.com>
2019-01-21 21:22:46 +0700

    add package

---
+++ KelasTerbuka/Python Dasar/package/main.py
@@ -0,0 +1,7 @@
+import  sains
+
+tambah = sains.tambah(3,4)
+print(tambah)
+
+kecepatan = sains.kecepatan(8,10)
+print(kecepatan)
---
+++ KelasTerbuka/Python Dasar/package/sains/fisika.py
@@ -0,0 +1,8 @@
+def kecepatan(waktu, jarak):
+    print("fungsi kecepatan")
+    return jarak /waktu
+
+def WaktuTempuh(kecepatan, jarak):
+    print("funsi waktu tempuh")
+    return jarak/kecepatan
+
